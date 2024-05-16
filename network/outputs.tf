c1aac75ae18927f7cea77ada87581ea9ac548a8f
ned1313 <ned.bellavance@gmail.com>
2024-05-16 08:17:06 -0400

    aks and network working

---
+++ network/outputs.tf
@@ -0,0 +1,14 @@
+output "subnet_ids" {
+  description = "Map of subnet names to IDs."
+  value       = module.network.vnet_subnets_name_id
+}
+
+output "aks_subnet_id" {
+  description = "ID of the subnet to deploy the AKS cluster into."
+  value       = module.network.vnet_subnets_name_id["aks"]
+}
+
+output "vnet_id" {
+  description = "VNet ID."
+  value       = module.network.vnet_id
+}
